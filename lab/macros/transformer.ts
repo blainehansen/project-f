5546bdcd43838daf9983ed3fd47ceff84dfd1019
Blaine Hansen <faichenshing@gmail.com>
2020-03-15 20:56:33 -0600

    the real problem is creating a transforming compiler that type checks the transformed code

--- lab/macros/transformer.ts
+++ lab/macros/transformer.ts
@@ -1,21 +1,33 @@
 import * as ts from 'typescript'
 
 const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
-	const visitor: ts.Visitor = node => transform(node) || ts.visitEachChild(node, visitor, context)
-	return sourceFile => ts.visitNode(sourceFile, visitor)
+	// const typeChecker = program.getTypeChecker
+
+	// const visitor: ts.Visitor = node => transform(node) || ts.visitEachChild(node, visitor, context)
+	// return sourceFile => ts.visitNode(sourceFile, visitor)
+	return sourceFile => {
+		return ts.updateSourceFileNode(sourceFile, [
+			ts.createExpressionStatement(ts.createIdentifier('stuff')),
+		  // Ensures the rest of the source files statements are still defined.
+		  ...sourceFile.statements,
+		])
+	}
 }
 export default transformer
 
 
 
-function transform(node: ts.Node) {
-	if (ts.isIdentifier(node)) {
-		switch (node.escapedText) {
-			case 'babel':
-				return ts.createIdentifier('typescript')
+// function transform(node: ts.Node, typeChecker: ts.TypeChecker) {
+// function transform(node: ts.Node) {
+// 	// typeChecker.getSymbolAtLocation()
+// 	// ts.getAliasedSymbol
+// 	if (ts.isIdentifier(node)) {
+// 		switch (node.escapedText) {
+// 			case 'babel':
+// 				return ts.createIdentifier('typescript')
 
-			case 'plugins':
-				return ts.createIdentifier('transformers')
-		}
-	}
-}
+// 			case 'plugins':
+// 				return ts.createIdentifier('transformers')
+// 		}
+// 	}
+// }
