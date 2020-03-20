9fbab30a104355d23ae430fd64de339b02d83d95
Blaine Hansen <faichenshing@gmail.com>
2020-03-19 23:09:11 -0600

    got the little html file working to play around with things

---
+++ lib/dom/index.ts
@@ -0,0 +1,16 @@
+// const svgNS = 'http://www.w3.org/2000/svg' as const
+// document.createElementNS
+
+// type SvgName = Parameters<typeof document.createElementNS>[1]
+// function createSvg<E extends SvgName>(element: E) {
+//     return document.createElementNS(svgNS, element)
+// }
+
+// const el = createSvg('circle')
+
+(window as any).main = function() {
+	const div = document.createElement('div')
+	div.textContent = 'yo yo yo'
+
+	document.body.appendChild(div)
+}
---
+++ lib/dom/notes.md
@@ -0,0 +1,3 @@
+https://dev.to/grandemayta/javascript-dom-manipulation-to-improve-performance-459a
+https://developer.mozilla.org/en-US/docs/Web/API/Document/createDocumentFragment
+https://javascript.info/basic-dom-node-properties
